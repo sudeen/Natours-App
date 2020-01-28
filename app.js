ad97aba52f07c51b265273edac85414162d8d5e8
sudeen <ranjitkarsudeen14@gmail.com>
2020-01-28 23:20:51 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); // middleware
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server side' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
