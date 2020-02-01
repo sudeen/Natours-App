4d56610debc8dc5b9d0cdfcd7c97c7eda3751646
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-01 23:13:56 +1100

    added morgan logging package

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-app.use(express.json()); // middleware
+// MIDDLEWARES
+app.use(morgan('dev')); // To get log in the console
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from the server side' });
@@ -13,6 +16,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//ROUTE HANDLERS
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -25,8 +29,10 @@ const getAllTours = (req, res) => {
 
 // app.get('/api/v1/tours', getAllTours);
 
+// ROUTES
 app.route('/api/v1/tours').get(getAllTours);
 
+// START SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
