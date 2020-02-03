6ab0ce44e161c38379047a0e503d97ebdf130909
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-04 00:10:48 +1100

    limiting fields in mongoose query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,26 +3,33 @@ const Tour = require('../models/tourModel');
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
+    // console.log(req.query);
     // BUILD QUERY
     // 1) FILTERING
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
-    // console.log(req.query, queryObj);
 
     // 2) ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
 
     // 3) SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
       query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 4) FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v')
     }
 
     //EXECUTE QUERY
