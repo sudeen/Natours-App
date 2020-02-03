e045fe3e3227abcfcc26e6c679b308372b19dd51
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-03 15:27:01 +1100

    database connected successfully

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev')); // To get log in the console
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // To get log in the console
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server side' });
-// });
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful'));
+
 // START SERVER
-const port = 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
