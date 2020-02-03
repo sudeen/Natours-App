2055bb2dc17f0cb4b2f33223cfedd09ce039d8ce
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-04 00:42:53 +1100

    aliasing using middleware to get top 5 cheap tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.ailasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
   try {
