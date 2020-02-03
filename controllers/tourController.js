f2afac794df60b800b1584078ff4d8dbe9e60589
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-04 00:30:54 +1100

    pagination included

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,7 +29,19 @@ exports.getAllTours = async (req, res) => {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
-      query = query.select('-__v')
+      query = query.select('-__v');
+    }
+
+    // 5) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //EXECUTE QUERY
