1bb95c4b3de6b8b4a1d4fd420daf2870b97f092e
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-04 03:46:51 +1100

    validator package installed

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equals 40 characters'],
       minlength: [10, 'A tour name must have more or equals 10 characters']
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
