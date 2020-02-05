931a916964f4e4ab699416c404f1da5a92642942
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-05 15:16:18 +1100

    handling uncaught exceptions feature added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+// Handling uncaught exceptions. This should be on top before any thing starts
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1); // Crashing the application is necessary here
+});
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -29,6 +36,9 @@ process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
-    process.exit(1);
+    process.exit(1); // Crashing the application is optional here
   });
 });
+
+// Examples of uncaught exception
+// console.log(x);
