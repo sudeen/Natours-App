8f84c0227c13ae4bdec0c1ab8239d7acfa607171
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-05 15:31:46 +1100

    user schema created

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please tell us your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmai, 'Please provide a valid email']
+  },
+  photo: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password']
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
