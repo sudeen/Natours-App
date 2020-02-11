8504ab52fa9ef1bd44f5050c7056c877a7269b0d
sudeen <ranjitkarsudeen14@gmail.com>
2020-02-11 23:48:05 +1100

    setting up the pug template (Server-side rendering)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 /* GLOBAL MIDDLEWARES */
+
+/* Serving static files */
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* Set Security HTTP headers */
 app.use(helmet());
@@ -58,9 +66,6 @@ app.use(
   })
 );
 
-/* Serving static files */
-app.use(express.static(`${__dirname}/public`));
-
 /* Test middlewares */
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -69,6 +74,13 @@ app.use((req, res, next) => {
 });
 
 /* ROUTES */
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Sudin'
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
